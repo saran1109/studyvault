554474fa7b6c871e76208f5c3c4d725680857bd3
saran1109 <gollapallisaran74@gmail.com>
2026-05-18 00:49:59 +0530

    fixed vercel routing

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,48 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp }
+from "firebase/app";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import {
+  getAuth,
+  GoogleAuthProvider
+}
+from "firebase/auth";
+
+import {
+  getFirestore
+}
+from "firebase/firestore";
+
 const firebaseConfig = {
+
   apiKey: "AIzaSyDXdWEcyc2rO54W09CJaFjSxBCVa7g_Qbw",
-  authDomain: "studyvault-170e9.firebaseapp.com",
-  projectId: "studyvault-170e9",
-  storageBucket: "studyvault-170e9.firebasestorage.app",
-  messagingSenderId: "270035683799",
-  appId: "1:270035683799:web:90ea9b00212e33201c2392",
-  measurementId: "G-N2NP5DTZXJ"
+
+  authDomain:
+    "studyvault-170e9.firebaseapp.com",
+
+  projectId:
+    "studyvault-170e9",
+
+  storageBucket:
+    "studyvault-170e9.appspot.com",
+
+  messagingSenderId:
+    "270035683799",
+
+  appId:
+    "1:270035683799:web:90ea9b00212e33201c2392"
+
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const app =
+  initializeApp(firebaseConfig);
+
+export const auth =
+  getAuth(app);
+
+export const provider =
+  new GoogleAuthProvider();
+
+export const db =
+  getFirestore(app);
+
+export default app;
